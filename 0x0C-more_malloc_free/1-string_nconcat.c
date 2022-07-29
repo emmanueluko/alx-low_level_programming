015b49b6fe9511d3c02cc1b92c70eb9ba8e1f322
Emma Uko <uko.emmanuel3@gmail.com>
2022-07-29 05:29:35 +0100

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  * @n: amount of bytes.
  *
  * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * if malloc fails, status value is equal to 98..
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
